1c066d7ebd8981a840aa0e63985769a0e5930485
zara ahmadi <zatokey@gmail.com>
2020-05-10 19:10:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 
-def square_array(array)
-  i = 0
- while array[i] do 
- 
-  array.push(array[i]*array[i]) 
-  i += 1
- end
-end
+1.	counter = 0
+2.	array = [1,2,3,4,5]
+3.	 
+4.	while array[counter] do
+5.	puts array[counter] * array[counter]
+6.	counter += 1
+7.	end
