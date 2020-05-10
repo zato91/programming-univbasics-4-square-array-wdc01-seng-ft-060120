0fd845485d9a214483f67927647c082eff0eb3ef
zara ahmadi <zatokey@gmail.com>
2020-05-10 19:15:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,8 @@
-array = [1, 2, 3, 4, 5]
-array.each do |item|
-  x = item * item
-  return x
-end
+
+1.	i = 0
+2.	array = [1,2,3,4,5]
+3.	 
+4.	while i < array.length 
+5.	puts array[i] *  array[i] 
+6.	counter += 1
+7.	end
