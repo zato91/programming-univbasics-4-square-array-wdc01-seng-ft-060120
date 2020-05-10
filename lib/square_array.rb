07ff9af4e76d10d77d6280c4b39e41b13a6a0820
zara ahmadi <zatokey@gmail.com>
2020-05-10 19:17:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,15 @@
 1.	i = 0
 2.	array = [1,2,3,4,5]
 3.	 
-4.	while i < array.length 
-5.	puts array[i] *  array[i] 
-6.	counter += 1
-7.	end
+4.
+  while i < array.length
+   
+    result[i] = nmbers[i] ** 2
+
+    # and don't forget to increase the index,
+    # otherwise the loop will run forever.
+    i += 1
+  end
+
+  # Return the result array
+  result
